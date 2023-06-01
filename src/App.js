e96f3a04160032e7d09c97d503c74e0c2ae7b572
adalmando <adalmando.silva@dcx.ufpb.br>
2023-06-01 14:45:48 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import TaskList from './components/Tasklist/TaskList';
+import React, {useState} from 'react';
 
 
-function App() {
+// MODELO DA TASK (OBJETO):
+const task = {
+    id:0,
+    title: "Nova tarefa",
+    state: "pendente"
+};
+
+
+// MÉTODO QUE GERA ID's INCREMENTAL:
+let idAcc = 0;
+const generateId = () =>{
+  idAcc++;
+  return idAcc;
+};
+
+
+export default function App() {  
+
+  // CRIANDO UMA VARIAVEL/CONSTANTE TASKS E DIZENDO QUE ELA POSSUI ESTADO:
+  const [tasks, setTasks] = useState([]); 
+  
+  //CRIANDO MÉTODO CONSTRUTOR DE TASKS:
+  const addTask = (title, state) => {
+    const newTask = {
+      id: generateId(),
+      title,
+      state
+    };
+
+    // AGORA VOU ADICIONAR A TASK QUE CRIEI A LISTA DE TASKS:
+    setTasks((existingTasks) => {
+      return [...existingTasks, newTask];
+    })
+  };
+
   return (
     <div className="App">
       <Navbar />
-      <div>
-        <TaskList title= "Pendente" />
-        <TaskList title= "Em andamento" />
-        <TaskList title= "Finalizado" />
+      <div className='container'>
+        <TaskList title= "Pendente" onAddTask={addTask} tasks={tasks} />
       </div>
     </div>
   );
 }
 
-export default App;
--- src/components/Tasklist/TaskList.js
+++ src/components/Tasklist/TaskList.js
@@ -1,11 +1,28 @@
 import React from "react";
 import './tasklist.css';
+import PropTypes from 'prop-types';
+import TaskItem from "../taskitem/TaskItem";
 
-export default function TaskList({ title }){
+export default function TaskList({ title, onAddTask, tasks  }){
+    const addTask = () => {
+        onAddTask("Nova tarefa", "Pendente")
+    };
+
     return(
         <div className='tasklist'>
-            <div className="title">{title /*Abro chaves para dizer que isso é um .jsx*/}</div>
-            <div className="content"></div>
+            <div className="title">{title}</div>
+            <div className="content">
+                {tasks.map((task) => {
+                    return <TaskItem key={task.id} id={task.id} title={task.title} taskState={task.taskState} />
+                })}
+            </div>
+            <button onClick={addTask}>Adicionar tarefa</button>
         </div>
     );
+}
+
+TaskList.propTypes = {
+    title: PropTypes.string.isRequired,
+    onAddTask: PropTypes.func.isRequired,
+    tasks: PropTypes.array.isRequired
 }
